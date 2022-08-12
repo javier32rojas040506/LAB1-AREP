08a21d1e40fbf31e51725c6397415e29460ddaee
unknown <francisco.rojas-m@labinfo.is.escuelaing.edu.co>
2022-08-12 14:41:50 -0500

    spark web metho get

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -2,9 +2,10 @@ package edu.escuelaing.arep;
 
 import static spark.Spark.*;
 public class SparkWebApp {
-    public static void main(String[] args) {
+    public static void main() {
         port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
+        //get("/data", ((request, response) -> response.type("application/json");
     }
     static int getPort() {
         if (System.getenv("PORT") != null) {
