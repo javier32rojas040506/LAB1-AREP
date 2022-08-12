490b2d9ed898d3955e7546e22e277d4e6d3d4f86
unknown <francisco.rojas-m@labinfo.is.escuelaing.edu.co>
2022-08-12 13:00:56 -0500

    Proyecto completo.

--- src/main/java/edu/escuelaing/arep/SparkWebApp.java
+++ src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -1,6 +1,7 @@
+package edu.escuelaing.arep;
+
 import static spark.Spark.*;
 public class SparkWebApp {
-
     public static void main(String[] args) {
         port(getPort());
         get("/hello", (req, res) -> "Hello Heroku");
